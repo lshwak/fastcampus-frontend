b6253c81cb996e75650d123f1c3c2999fb4f198f
lshwak <ls_hwak@naver.com>
2023-10-23 22:03:50 +0900

    fc ts type inference

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,12 +1,7 @@
-interface User {
-  name: string,
-  age: number
-}
-interface Validation {
-  isValid: boolean
-}
-const herepy: User & Validation = {
-  name: 'Neo',
-  age: 35,
-  isValid: true
+// 초기화된 변수 `num`
+let num = 12
+
+// 기본값이 지정된 매개변수 `b` + 반환 값이 확실한 함수 `add`
+function add(a: number, b:number = 2) {
+  return a + b
 }

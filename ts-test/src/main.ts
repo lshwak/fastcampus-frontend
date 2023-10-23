a682712cc3b8d9df504131914b6b2ced15e85c22
lshwak <ls_hwak@naver.com>
2023-10-23 17:57:50 +0900

    fc type practice

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,5 +1,12 @@
-let hello: string = 'Hello world!'
-
-hello = 'Hello~'
-
-console.log(hello)
+interface User {
+  name: string,
+  age: number
+}
+interface Validation {
+  isValid: boolean
+}
+const herepy: User & Validation = {
+  name: 'Neo',
+  age: 35,
+  isValid: true
+}

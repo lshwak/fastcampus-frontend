f235cff15070a8d46b4a8c2510e31768bbfc96c7
lshwak <ls_hwak@naver.com>
2023-10-26 17:13:07 +0900

    fc ts assertions

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,10 +1,15 @@
 // 1)
-const el = document.querySelector('body')
-el.textContent = 'Hello world!'
+const el = document.querySelector('.title')
+if (el) {
+  el.textContent = 'Hello world!'
+}
+
 
 // 2)
 function getNumber(x: number | null | undefined) {
-  return Number(x?.toFixed(2)
+  if (x) {
+    return Number(x!.toFixed(2))
+  }
 }
 getNumber(3.1415926535)
 getNumber(null)
@@ -12,9 +17,13 @@ getNumber(null)
 // 3)
 function getValue(x: string | number, isNumber: boolean) {
   if (isNumber) {
-    return Number(x.toFixed(2))
+    return Number((x as number).toFixed(2))
   }
   return (x as string).toUpperCase()
 }
 getValue('hello world', false)  // 'HELLO WORLD'
 getValue(3.1415926535, true)  // 3.14
+
+let num!: number
+console.log(num)
+num = 123

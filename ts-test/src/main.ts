4d519b651d5ddc8ad444487dac372f4e8d83044b
lshwak <ls_hwak@naver.com>
2023-10-30 21:57:01 +0900

    fc ts index signiture payload logValues

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,18 +1,21 @@
-  interface Fruits {
-    [item: number]: string
+interface Payload {
+  [key: string]: unknown
+}
+function logValues(payload: Payload) {
+  for (const key in payload) {
+    console.log(payload[key])
   }
-  const fruits: Fruits = ['Apple', 'Banana', 'Cherry']
-  console.log(fruits[1])
+}
 
-  interface User {
-    [key: string]: unknown
-    name: string
-    age: number
-  }
-  const heropy: User = {
-    name: 'Heropy',
-    age: 35
-  }
-  heropy['isValid'] = true
-  heropy['email'] = ['thesecon@gmail.com', 'test@gmail.com']
-  console.log(heropy)
+interface User {
+  [key: string]: unknown
+  name: string
+  age: number
+  isValid: boolean
+}
+const heropy: User = {
+  name: 'Heropy',
+  age: 35,
+  isValid: true
+}
+logValues(heropy)

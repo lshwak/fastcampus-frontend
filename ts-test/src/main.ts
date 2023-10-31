fc48e54a2362d2db96f5a90edc89f7f40dbfa122
lshwak <ls_hwak@naver.com>
2023-10-31 22:25:33 +0900

    fc ts function overloading

--- ts-test/src/main.ts
+++ ts-test/src/main.ts
@@ -1,13 +1,20 @@
-interface Cat {
-  name: string
-  age: number
+function add1(a: string, b: string) {
+  return a + b
 }
-const cat: Cat = {
-  name: 'Lucy',
-  age: 3
+function add2(a: number, b: number) {
+  return a + b
 }
+add1('hello', 'world') // 'hello world'
+add2(1, 2) // 3
+add1('hello', 2)
+add2('hello', 2)
 
-function hello(this: Cat, message: string) {
-  console.log(`Hello ${this.name}, ${message}`)
+function add(a: string, b: string): string
+function add(a: number, b: number): number
+function add(a: any, b: any) {
+  return a + b
 }
-hello.call(cat, 'You are pretty awesome!') 
+add('hello', 'world') // 'hello world'
+add(1, 2) // 3
+add('hello', 2)
+add(1, 'world')

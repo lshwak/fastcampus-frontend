b7be4e4b9f93bcbc663b78245d174d31d748f01d
lshwak <ls_hwak@naver.com>
2023-11-13 21:31:13 +0900

    fc ts heropy.ts

--- vanillajs-movie-app/src/core/heropy.ts
+++ vanillajs-movie-app/src/core/heropy.ts
@@ -1,11 +1,24 @@
 ///// Component /////
+interface ComponentPayload {
+  tagName?: string
+  props?: {
+    [key: string]: unknown
+  }
+  state?: {
+    [key: string]: unknown
+  }
+}
+
 export class Component {
-  constructor(payload = {}) {
+  public el
+  public props
+  public state
+  constructor(payload:ComponentPayload = {}) {
     const {
       tagName = 'div', // 최상위 요소의 태그 이름
       props = {},
       state = {}
-    } = payload
+    } = payload 
     this.el = document.createElement(tagName) // 컴포넌트의 최상위 요소
     this.props = props // 컴포넌트가 사용될 때 부모 컴포넌트에서 받는 데이터
     this.state = state // 컴포넌트 안에서 사용할 데이터

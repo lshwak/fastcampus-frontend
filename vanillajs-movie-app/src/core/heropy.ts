4a490dfdcde4afaccad3b02251618878b3ed368d
lshwak <ls_hwak@naver.com>
2023-11-15 17:32:09 +0900

    fc ts Heropy.ts Store

--- vanillajs-movie-app/src/core/heropy.ts
+++ vanillajs-movie-app/src/core/heropy.ts
@@ -82,12 +82,18 @@ export function createRouter(routes: Routes) {
   }
 }
 
-
 ///// Store /////
-export class Store {
-  constructor(state) {
-    this.state = {} // 상태(데이터)
-    this.observers = {}
+interface StoreObservers {
+  [key: string]: SubscribeCallBack[]
+}
+interface SubscribeCallBack {
+  (arg: unknown): void
+}
+
+export class Store<S> {
+  public state = {} as S// 상태(데이터)
+  private observers = {} as StoreObservers
+  constructor(state: S) {
     for (const key in state) {
       // 각 상태에 대한 변경 감시(Setter) 설정!
       Object.defineProperty(this.state, key, {
@@ -104,7 +110,7 @@ export class Store {
     }
   }
   // 상태 변경 구독!
-  subscribe(key, cb) {
+  subscribe(key: string, cb: SubscribeCallBack) {
     Array.isArray(this.observers[key]) // 이미 등록된 콜백이 있는지 확인!
       ? this.observers[key].push(cb) // 있으면 새로운 콜백 밀어넣기!
       : this.observers[key] = [cb] // 없으면 콜백 배열로 할당!

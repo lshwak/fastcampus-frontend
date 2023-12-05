8b3a2d6474ba35230f63e38822cc66a035b4a641
lshwak <ls_hwak@naver.com>
2023-12-05 17:20:15 +0900

    fc react ttt handleClick setState

--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -11,8 +11,15 @@ import "./Board.css"
     }
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares : squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />
+    return <Square value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)} />
   }
 
    render() {
--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -5,7 +5,8 @@ export default class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
+      <button className="square" 
+        onClick={() => { this.props.onClick() }}>
         {this.props.value}
       </button>
     )

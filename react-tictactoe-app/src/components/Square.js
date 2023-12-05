d304fb85af7a11df2009523582d7037eb6129751
lshwak <ls_hwak@naver.com>
2023-12-05 15:38:30 +0900

    fc react ttt Board & Square squares state

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -3,17 +3,10 @@ import "./Square.css"
 
 export default class Square extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
   render() {
     return (
       <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }

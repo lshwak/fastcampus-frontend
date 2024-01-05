47a8a5ffb38d02df2b0b9732764005df60528c84
lshwak <ls_hwak@naver.com>
2024-01-05 14:34:49 +0900

    fc react disney image Banner UI truncate substring

--- react-disney-plus-app/src/components/Banner.js
+++ react-disney-plus-app/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios';
 import requests from '../api/request';
+import "./Banner.css";
 
 const Banner = () => {
 
@@ -24,8 +25,39 @@ const Banner = () => {
     setMovie(movieDetail);
   }
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  }
+
   return (
-    <div>Banner</div>
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover"
+      }}
+    >
+      <div className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key &&
+            <button
+              className='banner__button play'
+            >
+              Play
+            </button>
+          }
+        </div>
+        <p className='banner__description'>
+          {truncate(movie.overview, 100)}
+        </p>
+      </div>
+      <div className='banner--fadeBottom' />
+    </header>
   )
 }
 

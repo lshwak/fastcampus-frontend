b2f393637737c3632bb455b020c99edbd50c1cd4
lshwak <ls_hwak@naver.com>
2024-03-10 15:46:41 +0900

    fc react disney login page

--- react-disney-plus-app/src/pages/LoginPage/index.js
+++ react-disney-plus-app/src/pages/LoginPage/index.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Container>
+      <Content>
+        <Center>
+          <LogoOne src='/images/cta-logo-one.svg' alt='logo-one' />
+          <SignUpLink>지금 가입</SignUpLink>
+          <Description>
+            영화에 대한 프리미어 엑세스를 얻으십시오.
+            디즈니 플러스 가격은 다음 주부터 1000원 인상됩니다.
+          </Description>
+          <LogoTwo src='/images/cta-logo-two.svg' alt='logo-two' />
+        </Center>
+      </Content>
+    </Container>
   )
 }
 
 export default LoginPage
+
+const Container = styled.section``;
+
+const Content = styled.div``;
+
+const Center = styled.div``;
+
+const LogoOne = styled.img``;
+
+const SignUpLink = styled.a``;
+
+const Description = styled.p``;
+
+const LogoTwo = styled.img``;

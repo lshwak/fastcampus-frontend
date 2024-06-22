26e00acbaefb6a616bcac27ddb14602f32190f77
lshwak <ls_hwak@naver.com>
2024-06-22 14:03:20 +0900

    fc react options

--- react-context-app/client/src/components/Type.js
+++ react-context-app/client/src/components/Type.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Products from './Products';
 import Options from './Options';
+import ErrorBanner from './ErrorBanner';
 
 const Type = ({ orderType }) => {
 
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadItems(orderType)
@@ -15,8 +17,9 @@ const Type = ({ orderType }) => {
     try {
       const response = await axios.get(`http://localhost:4000/${orderType}`);
       setItems(response.data);
+      // console.log('response.data',response.data);
     } catch (error) {
-      console.log(error)
+      setError(true);
     }
   }
 
@@ -29,6 +32,9 @@ const Type = ({ orderType }) => {
       imagePath={item.imagePath}
     />
   ))
+  if (error) {
+    return (<ErrorBanner message={"에러가 발생했습니다."} />)
+  }
 
   return (
     <div>
